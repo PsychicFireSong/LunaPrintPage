9b1d3ae98c8a84e8f250e9082170542047c8bae5
PsychicFireSong <mastervosuyg@gmail.com>
2025-11-19 00:30:29 +0800

    Proofread all pages, added Artwork page.

---
+++ artwork-data.js
@@ -0,0 +1,81 @@
+// Auto-generated artwork data
+// Illustrations are displayed in descending order (newest first)
+
+const illustrationsData = [
+    {
+        image: 'images/Artwork/Illustrations/9.png',
+        story: 'The last piece I commissioned for 2025 is a conbook submission for FurUM 2025. A collaborative effort, we (mostly the other 3) build a pirate scene to match the convention themes this year. It promises adventure, and to soar across new horizons in search of greatness, both in valor and in being gosh darn adorable.',
+        artists: [
+            { name: 'Me! (Concept, Sketches)', link: 'https://t.me/PsychicFireSong' },
+            { name: 'Thomas(Direction, Sketches)', link: null },
+            { name: 'Gatzz (Characters)', link: 'https://t.me/Gatzz_2' },
+            { name: 'AuroZephyr(Background)', link: 'https://t.me/AuroZephyr' }
+        ],
+        year: 2025
+    },
+    {
+        image: 'images/Artwork/Illustrations/8.png',
+        story: 'A beautiful piece given to me by my friend Ammy. This piece illustrates the character we share, nicknamed Kaei. They are a blend between Amaterasu from Okami, with the whims of the oceans as a Manokit. This piece shows the beauty of the character as the stand beneath blossoming Sakura trees.',
+        artists: [
+            { name: 'Qubrie(No Comms)', link: 'https://t.me/Qubrie' }
+        ],
+        year: 2025
+    },
+    {
+        image: 'images/Artwork/Illustrations/7.png',
+        story: 'The first of 3 major pieces I got in 2025, this is a large illustration of me and a good friend soaring high above the skies of the outlands plateau, a sanctuary where all will shimmer before us. The animated version is prominently shown at the top of my page.',
+        artists: [
+            { name: 'AuroZephyr', link: 'https://t.me/AuroZephyr' },
+            { name: 'Animated by Kerac', link: 'https://www.kerac-fa.com' }
+        ],
+        year: 2025
+    },
+    {
+        image: 'images/Artwork/Illustrations/6.png',
+        story: 'This is a photocard I commissioned for use in making namecards, and to get that pretty style as a profile picture. It\'s meant to show a cute, techno-punk aesthetic me, with poppy colours and an eye for style.',
+        artists: [
+            { name: 'Radishkek', link: 'https://t.me/radishkek' }
+        ],
+        year: 2025
+    },
+    {
+        image: 'images/Artwork/Illustrations/5.png',
+        story: 'This is one of two pieces I commissioned for a moment from LIFC 2024. This piece is an abstraction showing our team carrying our almighty floofy friend, inspiring awe and wonder.',
+        artists: [
+            { name: 'Fiddle', link: 'https://t.me/FiddleHBadger' }
+        ],
+        year: 2024
+    },
+    {
+        image: 'images/Artwork/Illustrations/4.png',
+        story: 'This is one of two pieces I commissioned for a moment from LIFC 2024. This piece shows our team scoring big while carrying our small floofy friend to the finish line.',
+        artists: [
+            { name: 'Gatzz', link: 'https://t.me/Gatzz_2' }
+        ],
+        year: 2024
+    },
+    {
+        image: 'images/Artwork/Illustrations/3.png',
+        story: 'This is a piece i received as apart of a request to draw the feral variant of an older version of Luna. The expression requested was for my character to be derpy, and the artist did not disappoint.',
+        artists: [
+            { name: 'ApollyonDragon', link: 'https://www.furaffinity.net/gallery/apollyondragon/' }
+        ],
+        year: 2023
+    },
+    {
+        image: 'images/Artwork/Illustrations/2.png',
+        story: 'This is a challenge portrait piece, where the artist had to get creative to blend the aspects of my Jetpony Aerie, with the features of a fennec fox. The result is a cute flying anthro, whom I quite like.',
+        artists: [
+            { name: 'CraftRabbit', link: 'http://twitter.com/craftrabbit2063' }
+        ],
+        year: 2023
+    },
+    {
+        image: 'images/Artwork/Illustrations/1.png',
+        story: 'This is a fight scene I received in a services trade, showcasing a sparring match between me and a friend of mine, as others watched on. Drawn using watercolours on a large A2 paper, and scanned with pixel shift on my camera, the size of the full resolution of this illustration is a staggering 1.1GB.',
+        artists: [
+            { name: 'Clemaxgra', link: 'https://www.furaffinity.net/user/clemaxgra' }
+        ],
+        year: 2025
+    }
+];
